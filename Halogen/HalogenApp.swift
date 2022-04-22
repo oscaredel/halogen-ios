de07445110b9d823031289af43dccede154f2f1c
Oscar Edel <edeloscar@gmail.com>
2022-04-22 14:12:59 +0200

    Initial Commit

---
+++ Halogen/HalogenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalogenApp.swift
+//  Halogen
+//
+//  Created by Oscar Edel on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HalogenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
